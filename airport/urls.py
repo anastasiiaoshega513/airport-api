8401a9b3cf057eb4f38b45c51001417dc1a24558
Steicy <anastasiaosega@gmail.com>
2026-02-21 17:20:03 +0200

    Add initial serializers/viewsets/router skeletons

--- airport/urls.py
+++ airport/urls.py
@@ -1,7 +1,20 @@
-# from airport.views import ()
+from django.urls import path, include
 
-urlpatterns = [
+from airport.views import AirportViewSet, RouteViewSet, AirplaneTypeViewSet, AirplaneViewSet, CrewViewSet, OrderViewSet, \
+    FlightViewSet, TicketViewSet
 
-]
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register("airports", AirportViewSet)
+router.register("routes", RouteViewSet)
+router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("crews", CrewViewSet)
+router.register("orders", OrderViewSet)
+router.register("flights", FlightViewSet)
+router.register("tickets", TicketViewSet)
+
+urlpatterns = [path("", include(router.urls))]
 
 app_name = "airport"
