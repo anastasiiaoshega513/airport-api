2cc7332cacdfadebd8cff89790517e7402d09515
Steicy <anastasiaosega@gmail.com>
2026-02-22 21:51:54 +0200

    added validation to ticket

--- airport/models.py
+++ airport/models.py
@@ -112,5 +112,51 @@ class Ticket(models.Model):
         unique_together = ("flight", "row", "seat")
         ordering = ("row", "seat")
 
+
+    @staticmethod
+    def validate_ticket(row: int, seat: int, airplane: Airplane, error_to_raise):
+
+        for ticket_attr_value, ticket_attr_name, airplane_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(airplane, airplane_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                        f"number must be in available range: "
+                        f"(1, {airplane_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.flight.airplane,
+            ValidationError,
+        )
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+            *args,
+            **kwargs,
+    ):
+        self.full_clean()
+        return super().save(
+            *args,
+            **kwargs,
+            force_insert=force_insert,
+            force_update=force_update,
+            using=using,
+            update_fields=update_fields,
+        )
+
     def __str__(self):
         return f"Ticket to {self.flight}, order {self.order.id}"
