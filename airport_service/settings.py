9b63f9258e3ab3edd8431b4e2d8dcdb5473f6f77
Steicy <anastasiaosega@gmail.com>
2026-02-25 19:55:55 +0200

    Fix static root for Render

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -151,6 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 SPECTACULAR_SETTINGS = {
     "TITLE": "Airport API",
